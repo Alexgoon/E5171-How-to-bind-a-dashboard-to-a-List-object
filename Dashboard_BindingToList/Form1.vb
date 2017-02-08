9f7a449c93b6547ac78c1f532cf61c158f844fa0
Alexgoon <alex.russkovdx@gmail.com>
2017-02-08 15:22:48 +0300

    VB generationa18d31c6-bb9f-4314-9c39-036080d09795

--- Dashboard_BindingToList/Form1.vb
+++ Dashboard_BindingToList/Form1.vb
@@ -7,7 +7,7 @@ Namespace Dashboard_BindingToList
 
         Public Sub New()
             InitializeComponent()
-            Dim t As Integer=77777
+            Dim t As Integer=77771
         End Sub
 
         Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
