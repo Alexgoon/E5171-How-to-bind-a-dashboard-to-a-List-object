9b2de4eb6c7808691c4ee0df513a2c8b1172c2c8
Alexgoon <alex.russkovdx@gmail.com>
2017-02-09 13:20:06 +0300

    Fix(VB)

--- Dashboard_BindingToList/Form1.vb
+++ Dashboard_BindingToList/Form1.vb
@@ -8,7 +8,7 @@ Namespace Dashboard_BindingToList
         Public Sub New()
             InitializeComponent()
             Dim t As Integer=7777123
-            Dim s As String = "hi"
+            Dim s As String = "fixed"
         End Sub
 
         Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
