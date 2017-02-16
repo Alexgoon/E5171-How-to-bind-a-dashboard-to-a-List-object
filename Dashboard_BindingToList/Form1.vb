c5b22f978f13a3e43b6d4e775aea266858383772
Alexgoon <alex.russkovdx@gmail.com>
2017-02-16 12:26:11 +0300

    T = 5(VB)

--- Dashboard_BindingToList/Form1.vb
+++ Dashboard_BindingToList/Form1.vb
@@ -7,7 +7,7 @@ Namespace Dashboard_BindingToList
 
 		Public Sub New()
 			InitializeComponent()
-			Dim t As Integer=4
+			Dim t As Integer=5
 		End Sub
 
 		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
