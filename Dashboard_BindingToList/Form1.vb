d9d29a2596c8395c26b9a8fc5333442c38b36dcf
Alexgoon <alex.russkovdx@gmail.com>
2017-02-17 14:45:29 +0300

    Increment t(VB)

--- Dashboard_BindingToList/Form1.vb
+++ Dashboard_BindingToList/Form1.vb
@@ -7,7 +7,8 @@ Namespace Dashboard_BindingToList
 
 		Public Sub New()
 			InitializeComponent()
-			Dim t As Integer=25
+			Dim t As Integer=256
+			t += 1
 		End Sub
 
 		Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
